ee1796b56065895c891b8e92a99d02b2c9499afc
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-13 11:49:19 -0700

    add password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,34 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  function generatePassword() {
+    const strings = {
+      lowers: "abcdefghijklmnopqrstuvwxyz",
+      uppers: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      numbers: "1234567890",
+      symbols: "`-=[]\;',./~!@#$%^&*()_+{}|:<>?`",
+    };
 
+    var passwordChars = "";
+
+    // create password length
+    var length = window.prompt(
+      "Enter a number from 8 to 128 for password length."
+    );
+
+    // confirm user wants to use lowercase
+
+
+    // confirm user wants to use uppercase
+
+    // confirm user wants to use numbers
+
+    // confirm user wants to use symbols
+    
+
+  }
+
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
