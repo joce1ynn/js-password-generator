cc215a06076adc44488ce584be320a83186aa3b7
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-13 20:20:29 -0700

    add generating password randomly

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,47 +17,48 @@ function generatePassword() {
     symbols: "`-=[];',./~!@#$%^&*()_+{}|:<>?`",
   };
 
-  // Ask user to put password length
+  // Ask users to put password length
   var length = 0;
   var askLength = true;
   while (askLength) {
     if (length < 8 || length > 128) {
       length = window.prompt(
-        "Enter a number from 8 to 128 for password length."
+        "Please enter a number from 8 to 128 for password length."
       );
     } else {
       askLength = false;
     }
   }
 
-  // Ask user to put password character
+  // Ask users to put password character detials
   var passwordChars = "";
   var askCharacter = true;
   while (askCharacter) {
-    // confirm user wants to use lowercase
+    // confirm users wants to use lowercase
     var lowercase = window.confirm("Would you like to use lowercase letters?");
     if (lowercase) {
       passwordChars += strings.lowers;
     }
 
-    // confirm user wants to use uppercase
+    // confirm users want to use uppercase
     var uppercase = window.confirm("Would you like to use uppercase letters?");
     if (uppercase) {
       passwordChars += strings.uppers;
     }
 
-    // confirm user wants to use numbers
+    // confirm users want to use numbers
     var numbers = window.confirm("Would you like to use numbers?");
     if (numbers) {
       passwordChars += strings.numbers;
     }
 
-    // confirm user wants to use symbols
+    // confirm users want to use symbols
     var symbols = window.confirm("Would you like to use special characters?");
     if (symbols) {
       passwordChars += strings.symbols;
     }
 
+    // will ask again if users didn't choose any of the character type
     if (!lowercase && !uppercase && !numbers && !symbols) {
       window.alert("At least one character type must be selected");
     } else {
@@ -65,6 +66,12 @@ function generatePassword() {
     }
   }
 
+  // Generate password randomly based on users' choice
+  var password = "";
+  for (let i = 0; i < length; i++) {
+    password += passwordChars[Math.floor(Math.random() * passwordChars.length)];
+  }
+  return password;
 }
 
 // Add event listener to generate button
