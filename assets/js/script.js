6542a8c3ae6b8f4b61079bd2cad08ec568921c72
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-13 20:04:31 -0700

    add password character

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,35 +5,66 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
-  function generatePassword() {
-    const strings = {
-      lowers: "abcdefghijklmnopqrstuvwxyz",
-      uppers: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-      numbers: "1234567890",
-      symbols: "`-=[]\;',./~!@#$%^&*()_+{}|:<>?`",
-    };
+// Define password character type
+function generatePassword() {
+  const strings = {
+    lowers: "abcdefghijklmnopqrstuvwxyz",
+    uppers: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "1234567890",
+    symbols: "`-=[];',./~!@#$%^&*()_+{}|:<>?`",
+  };
 
-    var passwordChars = "";
-
-    // create password length
-    var length = window.prompt(
-      "Enter a number from 8 to 128 for password length."
-    );
-
-    // confirm user wants to use lowercase
-
-
-    // confirm user wants to use uppercase
-
-    // confirm user wants to use numbers
-
-    // confirm user wants to use symbols
-    
-
+  // Ask user to put password length
+  var length = 0;
+  var askLength = true;
+  while (askLength) {
+    if (length < 8 || length > 128) {
+      length = window.prompt(
+        "Enter a number from 8 to 128 for password length."
+      );
+    } else {
+      askLength = false;
+    }
   }
 
-  passwordText.value = password;
+  // Ask user to put password character
+  var passwordChars = "";
+  var askCharacter = true;
+  while (askCharacter) {
+    // confirm user wants to use lowercase
+    var lowercase = window.confirm("Would you like to use lowercase letters?");
+    if (lowercase) {
+      passwordChars += strings.lowers;
+    }
+
+    // confirm user wants to use uppercase
+    var uppercase = window.confirm("Would you like to use uppercase letters?");
+    if (uppercase) {
+      passwordChars += strings.uppers;
+    }
+
+    // confirm user wants to use numbers
+    var numbers = window.confirm("Would you like to use numbers?");
+    if (numbers) {
+      passwordChars += strings.numbers;
+    }
+
+    // confirm user wants to use symbols
+    var symbols = window.confirm("Would you like to use special characters?");
+    if (symbols) {
+      passwordChars += strings.symbols;
+    }
+
+    if (!lowercase && !uppercase && !numbers && !symbols) {
+      window.alert("At least one character type must be selected");
+    } else {
+      askCharacter = false;
+    }
+  }
+
 }
 
 // Add event listener to generate button
